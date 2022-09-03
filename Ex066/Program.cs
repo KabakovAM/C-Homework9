f9d975004c1cf6f98d50968fc21d22f8cc241122
AntonK <ak@m.ru>
2022-09-03 10:40:06 +0300

    Задание выполнено

---
+++ Ex066/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу,
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int SumBetweenNumbers(int m, int n)
+{
+    if (m == n)
+        return m;
+    return m + SumBetweenNumbers(m + 1, n);
+}
+
+Console.Write("Введите натуральное число: ");
+int numberM = int.Parse(Console.ReadLine());
+Console.Write($"Введите натуральное число больше чем {numberM}: ");
+int numberN = int.Parse(Console.ReadLine());
+int result = SumBetweenNumbers(numberM, numberN);
+Console.Write($"Cумма натуральных элементов в промежутке от {numberM} до {numberN} равна {result}");
---
+++ Ex068/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии.
+// Даны два неотрицательных числа m и n.
+// m = 3, n = 2 -> A(m,n) = 29
+
+int Ackerman(int m, int n)
+{
+    if (m == 0) return n + 1;
+    if (m > 0 && n == 0) return Ackerman(m - 1, 1);
+    if (m > 0 && n > 0) return Ackerman(m - 1, Ackerman(m, n - 1));
+    return Ackerman(m, n);
+}
+
+Console.Write("Введите натуральное число m: ");
+int numberM = int.Parse(Console.ReadLine());
+Console.Write($"Введите натуральное число n: ");
+int numberN = int.Parse(Console.ReadLine());
+int result = Ackerman(numberM, numberN);
+Console.Write($"Значение функции Аккермана равно: {result}");
